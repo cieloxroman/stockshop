35a7d49d1bed84bfcc4e09875d6e3bfd76c25d9f
Cielo Roman <croman12@depaul.edu>
2023-04-12 22:50:13 -0500

    Updated Project2

--- app.js
+++ app.js
@@ -3,16 +3,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var layouts=require('express-ejs-layouts');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var contactRouter = require('./routes/contact');
 var aboutRouter = require('./routes/about');
+
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(layouts);
 
 app.use(logger('dev'));
 app.use(express.json());
